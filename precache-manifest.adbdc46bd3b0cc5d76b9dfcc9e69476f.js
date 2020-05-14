8f44675b7e935e6414faf08015905c1f8d20af70
AnastasiyaYutsevych <56453860+AnastasiyaYutsevych@users.noreply.github.com>
2020-05-14 08:13:34 -0400

    Updates

---
+++ precache-manifest.adbdc46bd3b0cc5d76b9dfcc9e69476f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0329d6a98e8d5d058ff3e33a00792bb1",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "5f9de4bc11bac973eae8",
+    "url": "/tictactoe/static/css/main.4abf1737.chunk.css"
+  },
+  {
+    "revision": "885087fbc97e2343c05a",
+    "url": "/tictactoe/static/js/2.25f9344f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.25f9344f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f9de4bc11bac973eae8",
+    "url": "/tictactoe/static/js/main.4bd80647.chunk.js"
+  },
+  {
+    "revision": "8cd3739712635f2cd5b8",
+    "url": "/tictactoe/static/js/runtime-main.148deea7.js"
+  }
+]);
